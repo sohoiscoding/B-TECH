4cb0ac148275efec7eab139d29fe3be41811cd46
Soheil Shokouhi <soheil.shokouhi@gmail.com>
2023-03-20 01:57:22 +0330

    javascript code added to the project

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('Hello form browser')
+const menuBtn = document.querySelector('#mobile-menu-btn')
+const menuList = document.querySelector('.menu')
+menuBtn.addEventListener('click',function(){
+    menuBtn.classList.toggle('active')
+    menuList.classList.toggle('active')
+})
